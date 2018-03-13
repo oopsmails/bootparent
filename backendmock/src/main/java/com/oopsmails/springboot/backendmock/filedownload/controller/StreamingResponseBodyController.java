a9a0c4f230624482de4e5342d44cdc38257004df
oopsmails <oopsmails@gmail.com>
2018-03-12 22:29:01 -0400

    testing logs: esapiLogger, log4j2, slf4j

--- backendmock/src/main/java/com/oopsmails/springboot/backendmock/filedownload/controller/StreamingResponseBodyController.java
+++ backendmock/src/main/java/com/oopsmails/springboot/backendmock/filedownload/controller/StreamingResponseBodyController.java
@@ -1,5 +1,8 @@
 package com.oopsmails.springboot.backendmock.filedownload.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.owasp.esapi.ESAPI;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
@@ -14,10 +17,26 @@ import java.io.InputStream;
 @RequestMapping("/backendmock")
 @CrossOrigin
 public class StreamingResponseBodyController {
+    private static final org.slf4j.Logger slf4jLogger = LoggerFactory.getLogger(StreamingResponseBodyController.class);
+
+    org.apache.logging.log4j.Logger log4jLogger = LogManager.getLogger(getClass());
+
+    private static final org.owasp.esapi.Logger esapiLogger = ESAPI.getLogger(StreamingResponseBodyController.class);
+
+
     @RequestMapping(value = "/downloadFile/{filetype}", method = { RequestMethod.GET, RequestMethod.POST })
     public StreamingResponseBody getSteamingFile(@PathVariable("filetype") String fileType,
                                                  @RequestParam("filename") String fileName,
                                                  HttpServletResponse response) throws IOException {
+        if (log4jLogger.isInfoEnabled()) {
+            log4jLogger.info("log4jLogger, fileName is {}", () -> replaceCrlr(fileName));
+        }
+
+        slf4jLogger.info("slf4jLogger, fileName is {}", fileName);
+
+        String testString = "\n\rabcd efg";
+        esapiLogger.info(null, testString.toString());
+
 
         response.setContentType(getContentType(fileType)); //"application/pdf"
         response.setHeader("Content-Disposition", getContentDisposition(fileName));
@@ -29,7 +48,7 @@ public class StreamingResponseBodyController {
             int nRead;
             byte[] data = new byte[1024];
             while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
-                System.out.println("Writing some bytes..");
+//                System.out.println("Writing some bytes..");
                 outputStream.write(data, 0, nRead);
             }
         };
@@ -52,5 +71,9 @@ public class StreamingResponseBodyController {
 
     private String getFileNameWithPath(String fileType, String fileName) {
         return "C:\\Temp\\MockbackendDownloadTest\\" + fileType + "\\" + fileName;
+    }
+
+    private String replaceCrlr(String fileName) {
+        return fileName.toString();
     }
 }
--- springjpa/src/main/java/com/oopsmails/spring/jpa/controller/UserController.java
+++ springjpa/src/main/java/com/oopsmails/spring/jpa/controller/UserController.java
@@ -3,6 +3,8 @@ package com.oopsmails.spring.jpa.controller;
 import com.oopsmails.spring.jpa.entity.User;
 import com.oopsmails.spring.jpa.repository.UserRepository;
 import java.util.List;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    private static final org.slf4j.Logger slf4jLogger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserRepository userRepository;
@@ -18,6 +21,7 @@ public class UserController {
     @RequestMapping(name = "get all users", method = RequestMethod.GET, value = "/users")
     List<User> findAllUsers() {
         Sort sort = new Sort(Sort.Direction.ASC, "id");
+        slf4jLogger.info("==================== Testing logging,  {}", sort);
         return userRepository.findAll(sort);
     }
 }
