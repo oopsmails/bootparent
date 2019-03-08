8d2560cd5e7a40671ffd9bb9c91fd7b2008c27da
oopsmails <oopsmails@gmail.com>
2019-03-07 22:32:05 -0500

    replacing 2d array

---
+++ backendmock/src/main/java/com/oopsmails/springboot/backendmock/domain/AttendanceRecord.java
@@ -0,0 +1,44 @@
+package com.oopsmails.springboot.backendmock.domain;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class AttendanceRecord {
+    private Map<String, ArrayList> attendanceRecordMap = new LinkedHashMap<String, ArrayList>();
+    private Set<String> participants = new LinkedHashSet<String>();
+    private Set<String> conferences = new LinkedHashSet<String>();
+
+    public AttendanceRecord() {
+    }
+
+    public Map<String, ArrayList> getAttendanceRecordMap() {
+        return attendanceRecordMap;
+    }
+
+    public Object[] getParticipantsArray() {
+        return participants.toArray();
+    }
+
+    public Object[] getConferencesArray() {
+
+        return conferences.toArray();
+    }
+
+    public void addToRecord(String title, String employee) {
+
+        conferences.add(title);
+        participants.add(employee);
+
+        if (attendanceRecordMap.containsKey(title)) {
+            ArrayList<String> tempList = attendanceRecordMap.get(title);
+            tempList.add(employee);
+        } else {
+            ArrayList<String> attendees = new ArrayList<String>();
+            attendees.add(employee);
+            attendanceRecordMap.put(title, attendees);
+        }
+    }
+}
